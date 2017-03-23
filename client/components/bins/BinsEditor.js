5f42d565b9fb2eadabbe4679a492841e6e6210c7
pip_lineupbuilder <howley.phil@gmail.com>
2017-03-23 11:39:05 -0700

    began working on markdown editor

--- client/components/bins/BinsEditor.js
+++ client/components/bins/BinsEditor.js
@@ -1,11 +1,21 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/markdown/markdown';
 
- class BinsEditor extends Component {
+class BinsEditor extends Component {
+  onEditorChange(content) {
+    console.log(content);
+  }
   render() {
     return (
-       <p>BinsEditor</p>
+       <div className="col-xs-8">
+         <h5>Input</h5>
+         <CodeMirror
+           onChange={this.onEditorChange.bind(this)}
+           options={{ mode: 'markdown', lineNumbers: true }} />
+       </div>
     );
   }
- }
+}
 
- export default BinsEditor;
+export default BinsEditor;
